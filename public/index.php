3315620a774839ac9724ddf4ef20564e0df64d0f
rivers-lis <rivers86li@gmail.com>
2018-04-29 17:33:43 +0300

    Create class Route

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \ishop\App();
-
-throw new Exception('Страница не найдена!!!', 404);
