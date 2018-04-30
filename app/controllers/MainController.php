093509e58e6ce2a9d55b566c6a8b38dc9fe1b5a7
rivers-lis <rivers86li@gmail.com>
2018-04-30 18:47:06 +0300

    Create base class View-Controller

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,8 @@
 
 namespace app\controllers;
 
+//use ishop\App;
+
 class MainController extends AppController {
 
 //    public $layout = 'test';
@@ -10,6 +12,14 @@ class MainController extends AppController {
     public function indexAction() {
 //        $this->layout = 'test';
         //echo __METHOD__;
+//        $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевики...');
+
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrey', 'Jane', 'Jo'];
+        $this->set(compact('name', 'age', 'names'));
     }
 
 }
