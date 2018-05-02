78a73bf1b2e7884c09ff6856368d9bc95af3bd9a
rivers-lis <rivers86li@gmail.com>
2018-05-02 19:51:17 +0300

    Create site

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,38 +1,16 @@
 <?php
 
-
 namespace app\controllers;
-
-//use ishop\App;
-
-use ishop\Cache;
 
 class MainController extends AppController {
 
-//    public $layout = 'test';
-
     public function indexAction() {
-//        $this->layout = 'test';
-        //echo __METHOD__;
-//        $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевики...');
 
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
-
+        $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands', 'hits'));
 
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jane', 'Jo', 'Mike'];
-        $cache = Cache::instance();
-//        $cache->set('test', $names);
-//        $cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data) {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 
 }
